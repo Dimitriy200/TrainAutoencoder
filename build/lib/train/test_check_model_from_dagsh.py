1c798cfefe8499c32a2a5fe8dd568ee5e864f3ea
Dmitriy <dikorpshy@mail.ru>
2025-01-07 14:04:09 +0300

    new version pydemic

---
+++ build/lib/train/test_check_model_from_dagsh.py
@@ -0,0 +1,13 @@
+import mlflow
+import dagshub
+import tensorflow as tf
+
+
+mlflow.set_tracking_uri("https://dagshub.com/Dimitriy200/diplom_autoencoder.mlflow")
+model_uri = f'models:/autoencoder2/latest'
+model = mlflow.keras.load_model(model_uri)
+
+
+print(model.summary())
+
+#
